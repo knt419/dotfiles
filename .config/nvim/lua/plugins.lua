d023483b2359a2a6a0f479dd05750e2e5fdf5abb
knt419 <knt419@gmail.com>
2023-04-08 03:02:20 +0900

    update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -429,7 +429,7 @@ local plugins = {
             g.lazygit_floating_window_winblend = 0
             g.lazygit_floating_window_scaling_factor = 0.9
             g.lazygit_floating_window_corner_chars = {"╭", "╮", "╰", "╯" }
-            g.lazygit_floating_window_use_plenary = 1
+            g.lazygit_floating_window_use_plenary = 0
         end,
         keys = {
             { "<Right>", "<Cmd>LazyGit<CR>" }
