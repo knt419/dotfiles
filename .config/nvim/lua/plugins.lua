115ee7e432c99819f3ce7d1e2ece795ca29909b9
knt419 <knt419@gmail.com>
2023-04-08 02:58:39 +0900

    update nvim-cmp config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -207,7 +207,7 @@ local plugins = {
                             key = 'p'
                         }
                     },
-                    project = { enable = false }
+                    project = { enable = true, limit = 8, action = 'Telescope find_files cwd=' }
                 }
             }
         end,
@@ -458,17 +458,20 @@ local plugins = {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require"lspconfig"
-            lspconfig.lua_ls.setup{}
-            lspconfig.sqlls.setup{}
-            lspconfig.bashls.setup{}
-            lspconfig.clangd.setup{}
+		local capabilities = require"cmp_nvim_lsp".default_capabilities()
+
+            lspconfig.lua_ls.setup{ capabilities = capabilities }
+            lspconfig.sqlls.setup{ capabilities = capabilities }
+            lspconfig.bashls.setup{ capabilities = capabilities }
+            lspconfig.clangd.setup{ capabilities = capabilities }
         end,
         keys = {
             { "<Leader>rf", "<Cmd>lua vim.lsp.buf.references()<CR>", silent = true },
             { "<Leader>df", "<Cmd>lua vim.lsp.buf.definition()<CR>", silent = true },
             { "<Leader>h", "<Cmd>lua vim.lsp.buf.hover()<CR>", silent = true },
             { "<Leader>f", "<Cmd>lua vim.lsp.buf.formatting()<CR>", mode = "x", silent = true }
-        }
+        },
+	  dependencies = {"hrsh7th/cmp-nvim-lsp"}
     },
     {"hrsh7th/cmp-nvim-lsp"},
     {"hrsh7th/cmp-buffer"},
@@ -482,6 +485,12 @@ local plugins = {
         config = function()
             local cmp = require"cmp"
             local lspkind = require"lspkind"
+
+            local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
 
             local feedkey = function(key, mode)
                 api.nvim_feedkeys(api.nvim_replace_termcodes(key, true, true, true), mode, true)
@@ -512,6 +521,8 @@ local plugins = {
                                 cmp.select_next_item()
                             elseif fn["vsnip#available"](1) == 1 then
                                 feedkey("<Plug>(vsnip-expand-or-jump)", "")
+                            elseif has_words_before() then
+                                cmp.complete()
                             else
                                 fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
                             end
