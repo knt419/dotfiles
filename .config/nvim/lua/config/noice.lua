ceaa84a6ddf2a5e37e75439e94fd83bb275fca85
knt419 <knt419@gmail.com>
2023-05-28 02:32:11 +0900

    update

---
+++ .config/nvim/lua/config/noice.lua
@@ -0,0 +1,23 @@
+return function()
+    require("noice").setup({
+        lsp = {
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true
+            }
+        },
+        routes = {
+            {
+                filter = {
+                    event = "msg_show",
+                    find = "%d+L, %d+B",
+                },
+                view = "mini",
+            },
+        },
+        presets = {
+            inc_rename = true
+        }
+    })
+end
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -169,29 +169,7 @@ local plugins = {
     {
         "folke/noice.nvim",
         event = {"CmdlineEnter", "BufNewFile", "BufRead"},
-        config = function()
-            require("noice").setup({
-                lsp = {
-                    override = {
-                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                        ["vim.lsp.util.stylize_markdown"] = true,
-                        ["cmp.entry.get_documentation"] = true
-                    }
-                },
-                routes = {
-                    {
-                        filter = {
-                            event = "msg_show",
-                            find = "%d+L, %d+B",
-                        },
-                        view = "mini",
-                    },
-                },
-                presets = {
-                    inc_rename = true
-                }
-            })
-        end,
+        config = require"config.noice",
         dependencies = {
             "MunifTanjim/nui.nvim",
             "rcarriga/nvim-notify"
