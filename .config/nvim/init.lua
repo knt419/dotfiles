939584f3cf1246d830e0934d032e4dcbb45da4be
knt419 <knt419@gmail.com>
2023-06-05 16:04:29 +0900

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,115 +12,115 @@
 vim.scriptencoding = "utf-8"
 vim.loader.enable()
 
-local fn = vim.fn
-local g = vim.g
-local opt = vim.opt
-local api = vim.api
-
 -- option
-opt.encoding = "utf-8"
-opt.fileencoding = "utf-8"
-opt.fileencodings = "iso-2022-jp,utf-8,cp932,euc-jp"
+require"config.option"
 
-opt.backup = false
-opt.writebackup = false
-opt.swapfile = false
-opt.backupdir:remove(".")
-opt.history = 50
-opt.shada = [['10,<30,f0,s5]]
+-- local g = vim.g
+-- local opt = vim.opt
 
-opt.autoread = false
-opt.switchbuf = "useopen"
-opt.confirm = true
-opt.number = true
-opt.signcolumn = "yes"
-opt.display = "uhex,lastline"
-opt.scrolloff = 5
-opt.foldmethod = "marker"
-opt.foldenable = true
-opt.foldlevelstart = 10
-opt.lazyredraw = false
-opt.termguicolors = true
+-- opt.encoding = "utf-8"
+-- opt.fileencoding = "utf-8"
+-- opt.fileencodings = "iso-2022-jp,utf-8,cp932,euc-jp"
 
-opt.guifont = "HackGen Console NF:h13"
-opt.guifontwide = "HackGen Console NF:h13"
-opt.emoji = true
-opt.showmatch = false
-opt.tabstop = 4
-opt.expandtab = true
-opt.shiftwidth = 4
-opt.shiftround = true
-opt.smartindent = true
-opt.complete = {".", "w", "b"}
-opt.conceallevel = 0
-opt.helplang = "ja,en"
+-- opt.backup = false
+-- opt.writebackup = false
+-- opt.swapfile = false
+-- opt.backupdir:remove(".")
+-- opt.history = 50
+-- opt.shada = [['10,<30,f0,s5]]
 
-opt.clipboard:append{'unnamedplus'}
-opt.startofline = false
-opt.list = true
-opt.listchars = {tab = "» ", trail = "·", eol = "↲", extends = "»", precedes = "«", nbsp = "%"}
-opt.fillchars = {eob = "\u{0020}"}
-opt.virtualedit = "block"
-opt.whichwrap = "b,s,h,l,<,>,[,]"
-opt.mouse = "a"
-opt.synmaxcol = 300
-opt.updatetime = 300
+-- opt.autoread = false
+-- opt.switchbuf = "useopen"
+-- opt.confirm = true
+-- opt.number = true
+-- opt.signcolumn = "yes"
+-- opt.display = "uhex,lastline"
+-- opt.scrolloff = 5
+-- opt.foldmethod = "marker"
+-- opt.foldenable = true
+-- opt.foldlevelstart = 10
+-- opt.lazyredraw = false
+-- opt.termguicolors = true
 
-opt.ignorecase = true
-opt.smartcase = true
-opt.wrapscan = true
-opt.infercase = true
+-- opt.guifont = "HackGen Console NF:h13"
+-- opt.guifontwide = "HackGen Console NF:h13"
+-- opt.emoji = true
+-- opt.showmatch = false
+-- opt.tabstop = 4
+-- opt.expandtab = true
+-- opt.shiftwidth = 4
+-- opt.shiftround = true
+-- opt.smartindent = true
+-- opt.complete = {".", "w", "b"}
+-- opt.conceallevel = 0
+-- opt.helplang = "ja,en"
 
-opt.diffopt = {"filler", "vertical", "internal", algorithm = "histogram", "indent-heuristic"}
-opt.splitright = true
+-- opt.clipboard:append{'unnamedplus'}
+-- opt.startofline = false
+-- opt.list = true
+-- opt.listchars = {tab = "» ", trail = "·", eol = "↲", extends = "»", precedes = "«", nbsp = "%"}
+-- opt.fillchars = {eob = "\u{0020}"}
+-- opt.virtualedit = "block"
+-- opt.whichwrap = "b,s,h,l,<,>,[,]"
+-- opt.mouse = "a"
+-- opt.synmaxcol = 300
+-- opt.updatetime = 300
 
-opt.laststatus = 3
-opt.wildmode = {list = "full"}
-opt.wrap = true
-opt.showmode = false
-opt.iskeyword:append("-")
-opt.linebreak = true
-opt.breakindent = true
-opt.breakindentopt = {shift = 2}
-opt.showbreak = "↪"
-opt.tags = ""
+-- opt.ignorecase = true
+-- opt.smartcase = true
+-- opt.wrapscan = true
+-- opt.infercase = true
 
-opt.inccommand = "split"
-opt.pumblend = 10
-opt.winblend = 10
-opt.splitkeep = "screen"
+-- opt.diffopt = {"filler", "vertical", "internal", algorithm = "histogram", "indent-heuristic"}
+-- opt.splitright = true
 
-g.mapleader = " "
-g.is_windows = fn.has("win16") == 1 or fn.has("win32") == 1 or fn.has("win64") == 1
-g.loaded_python3_provider = 0
-g.loaded_ruby_provider = 0
-g.loaded_perl_provider = 0
-g.did_install_default_menus = 1
-g.did_install_syntax_menu = 1
-g.loaded_syntax_completion = 1
-g.loaded_sql_completion = 1
-g.skip_loading_mswin = 1
-g.did_indent_on = 1
+-- opt.laststatus = 3
+-- opt.wildmode = {list = "full"}
+-- opt.wrap = true
+-- opt.showmode = false
+-- opt.iskeyword:append("-")
+-- opt.linebreak = true
+-- opt.breakindent = true
+-- opt.breakindentopt = {shift = 2}
+-- opt.showbreak = "↪"
+-- opt.tags = ""
 
-if g.is_windows then
-    g.clipboard = {
-        name = "win32yank.exe",
-        copy = {
-            ['+'] = {'win32yank.exe', '-i', '--crlf'},
-            ['*'] = {'win32yank.exe', '-i', '--crlf'},
-        },
-        paste = {
-            ['+'] = {'win32yank.exe', '-o', '--lf'},
-            ['*'] = {'win32yank.exe', '-o', '--lf'},
-        }
-    }
-end
+-- opt.inccommand = "split"
+-- opt.pumblend = 10
+-- opt.winblend = 10
+-- opt.splitkeep = "screen"
+
+-- g.mapleader = " "
+-- g.is_windows = fn.has("win16") == 1 or fn.has("win32") == 1 or fn.has("win64") == 1
+-- g.loaded_python3_provider = 0
+-- g.loaded_ruby_provider = 0
+-- g.loaded_perl_provider = 0
+-- g.did_install_default_menus = 1
+-- g.did_install_syntax_menu = 1
+-- g.loaded_syntax_completion = 1
+-- g.loaded_sql_completion = 1
+-- g.skip_loading_mswin = 1
+-- g.did_indent_on = 1
+
+-- if g.is_windows then
+--     g.clipboard = {
+--         name = "win32yank.exe",
+--         copy = {
+--             ['+'] = {'win32yank.exe', '-i', '--crlf'},
+--             ['*'] = {'win32yank.exe', '-i', '--crlf'},
+--         },
+--         paste = {
+--             ['+'] = {'win32yank.exe', '-o', '--lf'},
+--             ['*'] = {'win32yank.exe', '-o', '--lf'},
+--         }
+--     }
+-- end
 
 -- load plugins
 require"plugins"
 
 -- autocmd
-api.nvim_create_autocmd("User", {
+vim.api.nvim_create_autocmd("User", {
     pattern = "LazyVimStarted",
     callback = function()
         require"config.keymap"
---
+++ .config/nvim/lua/config/option.lua
@@ -0,0 +1,100 @@
+local g = vim.g
+local opt = vim.opt
+
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+opt.fileencodings = "iso-2022-jp,utf-8,cp932,euc-jp"
+
+opt.backup = false
+opt.writebackup = false
+opt.swapfile = false
+opt.backupdir:remove(".")
+opt.history = 50
+opt.shada = [['10,<30,f0,s5]]
+
+opt.autoread = false
+opt.switchbuf = "useopen"
+opt.confirm = true
+opt.number = true
+opt.signcolumn = "yes"
+opt.display = "uhex,lastline"
+opt.scrolloff = 5
+opt.foldmethod = "marker"
+opt.foldenable = true
+opt.foldlevelstart = 10
+opt.lazyredraw = false
+opt.termguicolors = true
+
+opt.guifont = "HackGen Console NF:h13"
+opt.guifontwide = "HackGen Console NF:h13"
+opt.emoji = true
+opt.showmatch = false
+opt.tabstop = 4
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.shiftround = true
+opt.smartindent = true
+opt.complete = {".", "w", "b"}
+opt.conceallevel = 0
+opt.helplang = "ja,en"
+
+opt.clipboard:append{'unnamedplus'}
+opt.startofline = false
+opt.list = true
+opt.listchars = {tab = "» ", trail = "·", eol = "↲", extends = "»", precedes = "«", nbsp = "%"}
+opt.fillchars = {eob = "\u{0020}"}
+opt.virtualedit = "block"
+opt.whichwrap = "b,s,h,l,<,>,[,]"
+opt.mouse = "a"
+opt.synmaxcol = 300
+opt.updatetime = 300
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.wrapscan = true
+opt.infercase = true
+
+opt.diffopt = {"filler", "vertical", "internal", algorithm = "histogram", "indent-heuristic"}
+opt.splitright = true
+
+opt.laststatus = 3
+opt.wildmode = {list = "full"}
+opt.wrap = true
+opt.showmode = false
+opt.iskeyword:append("-")
+opt.linebreak = true
+opt.breakindent = true
+opt.breakindentopt = {shift = 2}
+opt.showbreak = "↪"
+opt.tags = ""
+
+opt.inccommand = "split"
+opt.pumblend = 10
+opt.winblend = 10
+opt.splitkeep = "screen"
+
+g.mapleader = " "
+g.is_windows = vim.fn.has("win64") == 1
+g.loaded_python3_provider = 0
+g.loaded_ruby_provider = 0
+g.loaded_perl_provider = 0
+g.did_install_default_menus = 1
+g.did_install_syntax_menu = 1
+g.loaded_syntax_completion = 1
+g.loaded_sql_completion = 1
+g.skip_loading_mswin = 1
+g.did_indent_on = 1
+
+if g.is_windows then
+    g.clipboard = {
+        name = "win32yank.exe",
+        copy = {
+            ['+'] = {'win32yank.exe', '-i', '--crlf'},
+            ['*'] = {'win32yank.exe', '-i', '--crlf'},
+        },
+        paste = {
+            ['+'] = {'win32yank.exe', '-o', '--lf'},
+            ['*'] = {'win32yank.exe', '-o', '--lf'},
+        }
+    }
+end
