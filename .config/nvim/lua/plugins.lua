631c8afc60f40c7e70bf329489d8f67656e8c97d
knt419 <knt419@gmail.com>
2023-07-06 23:46:10 +0900

    update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -223,12 +223,21 @@ local plugins = {
         'numToStr/FTerm.nvim',
         config = function ()
             local shcmd = vim.env.SHELL or 'nu'
+            local visual
+            if not vim.env.NVIM then
+                visual = 'nvim --remote'
+            else
+                visual = 'nvim --server ' .. vim.env.NVIM .. ' --remote'
+            end
             require('FTerm').setup {
                 cmd = shcmd,
                 blend = 10,
                 dimensions = {
                     height = 0.9,
                     width = 0.9,
+                },
+                env = {
+                    VISUAL = visual,
                 },
             }
             vim.api.nvim_create_user_command('FtermLazygitOpen', function ()
