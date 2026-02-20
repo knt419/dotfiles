d98e21725e8a693f030bc2006025be2b91dfd00f
knt419 <knt419@gmail.com>
2026-02-20 10:49:33 +0900

    update setting for luasnip

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -404,6 +404,7 @@ local plugins = {
             {
                 'L3MON4D3/LuaSnip',
                 version = 'v2.*',
+                build = "make install_jsregexp",
             },
         },
         event = { 'InsertEnter', 'CmdlineEnter' },
