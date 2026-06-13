9d452a351fbb87c3589e4bf0ae12b0681f302e02
knt419 <knt419@gmail.com>
2026-06-13 20:17:07 +0900

    update

--- .config/nvim/lua/config/snacks.lua
+++ .config/nvim/lua/config/snacks.lua
@@ -14,7 +14,18 @@ return {
                     ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║        ████     █  █     ████ ',
                     ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝        ▀███▄            ▄███▀ ',
                     '                                                                  ▀▀████████████▀▀    '
-                }, '\n')
+                }, '\n'),
+            keys = {
+                { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                { icon = " ", key = "y", desc = "File Browse", action = '<cmd>Yazi<cr>' },
+                { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+            },
         },
         sections = {
             { section = 'header' },
